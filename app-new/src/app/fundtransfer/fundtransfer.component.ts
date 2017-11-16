dab7099f46e63e7cb3e70b5412cd479559ca285b
vishalkmohan <vishalkmohan@gmail.com>
2017-11-16 10:35:01 +0530

    [Fund transfer Form] foram control , navigation simple validation

--- app-new/src/app/fundtransfer/fundtransfer.component.ts
+++ app-new/src/app/fundtransfer/fundtransfer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-fundtransfer',
@@ -8,9 +10,25 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class FundtransferComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+  private transferForm:FormGroup;
+
 
   ngOnInit() {
+  	this.transferForm=new FormGroup({
+		 	accno: new FormControl("",[Validators.required]),
+		 	reaccno: new FormControl("",[Validators.required]),
+		 	acctype: new FormControl("",[Validators.required]),
+		 	ifsc: new FormControl("",[Validators.required]),
+		 	name: new FormControl("",[Validators.required]),
+		 	email: new FormControl("",[Validators.required])
+		 });
+  }
+
+  transfer=function(transferDeatils){
+  	console.log(transferDeatils.accno);
+  	this.router.navigateByUrl('/success');
   }
 
 }
