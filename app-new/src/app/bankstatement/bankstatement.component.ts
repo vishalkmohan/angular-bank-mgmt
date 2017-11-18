e54bc969ae398d4405074db1f7a93e20c618c1f7
vishalkmohan <vishalkmohan@gmail.com>
2017-11-18 11:05:56 +0530

    [Available balance from bank service]

--- app-new/src/app/bankstatement/bankstatement.component.ts
+++ app-new/src/app/bankstatement/bankstatement.component.ts
@@ -3,22 +3,25 @@ import { ActivatedRoute } from '@angular/router';
 
 import { StatementService } from './statement.service';
 import { MiniStatement } from './../domain/mini-statement';
+import { BankAccountService } from './../bankaccount/bank-account.service';
+import { Account } from './../domain/account';
 
 @Component({
   selector: 'app-bankstatement',
   templateUrl: './bankstatement.component.html',
   styleUrls: ['./bankstatement.component.css'],
   encapsulation: ViewEncapsulation.None,
-  providers:[StatementService]
+  providers:[StatementService,BankAccountService]
 })
 export class BankstatementComponent implements OnInit {
 
-   constructor(private route: ActivatedRoute,private statementService:StatementService) { }
+   constructor(private route: ActivatedRoute,private statementService:StatementService,private bankService:BankAccountService) { }
 
    accNo:string;
    sub:any;
 
    statementList: MiniStatement[] = [];
+   accountList: Account[] = [];
    totalCr:number=0;
    totalDr:number=0;
    total:number=0;
@@ -32,8 +35,9 @@ export class BankstatementComponent implements OnInit {
 
     //Retriving mini statements from service
     this.statementList=this.statementService.getMiniStatements(this.accNo);
+    this.accountList=this.bankService.getAccountDeatils().filter(x=>x.accountNo==this.accNo);
 
-    this.total= (this.statementList==null || this.statementList.length==0 ) ? 0:this.statementList.map(x=>x.amount).reduce((sum, current) => sum + current);
+    this.total= (this.accountList==null || this.accountList.length==0 ) ? 0:this.accountList.map(x=>x.balance).reduce((sum, current) => sum + current);
     this.totalCr= (this.statementList==null || this.statementList.length==0 ) ? 0:this.statementList.filter(x=>x.transactionType=="Cr").map(x=>x.amount).reduce((sum, current) => sum + current);
     this.totalDr= (this.statementList==null || this.statementList.length==0 ) ? 0:this.statementList.filter(x=>x.transactionType=="Dr").map(x=>x.amount).reduce((sum, current) => sum + current);
 
