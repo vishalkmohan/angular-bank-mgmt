c1459afab2e5150a0ec0e9cc5c234fb15c9f4d62
vishalkmohan <vishalkmohan@gmail.com>
2017-11-18 10:20:37 +0530

    [MiniStatement domain class]

--- app-new/src/app/bankstatement/bankstatement.component.ts
+++ app-new/src/app/bankstatement/bankstatement.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { StatementService } from './statement.service';
+import { MiniStatement } from './../domain/mini-statement';
 
 @Component({
   selector: 'app-bankstatement',
@@ -17,13 +18,19 @@ export class BankstatementComponent implements OnInit {
    accNo:string;
    sub:any;
 
+   statementList: MiniStatement[] = [];
+   totalCr:number=0;
+   totalDr:number=0;
+
+
   ngOnInit() {
   	this.sub = this.route.params.subscribe(params => {
        this.accNo = params['accNo']; 
     });
     console.log(this.accNo);
 
-    
+    //Retriving mini statements from service
+
   }
 
 }
---
+++ app-new/src/app/domain/mini-statement.ts
@@ -0,0 +1,14 @@
+export class MiniStatement {
+
+	statementId:string;
+	transactionDt:string;
+	transactionType:string;
+	amount:number;
+
+	constructor(statementId:strig,transactionDt:string,transactionType:string,amount:number){
+		this.statementId = statementId;
+		this.transactionDt = transactionDt;
+		this.transactionType = transactionType;
+		this.amount = amount;
+	}
+}
